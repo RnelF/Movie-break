a2974bbbf023138db456d8df9dac3da7744ab1f7
Arnel Francia <nelffff@gmail.com>
2024-08-17 22:29:17 +0800

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+require("dotenv").config({ path: "./key.env" });
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+});
