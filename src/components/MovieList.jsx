7f31ffd01544a90351bf075b39437c268514e658
Arnel Francia <nelffff@gmail.com>
2024-08-27 21:04:37 +0800

    updated some search result UI

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,7 +2,7 @@ import MovieItem from "./MovieItem.jsx";
 
 export default function MovieList({ movieData, setMovieId }) {
   return (
-    <div className="grid grid-cols-4">
+    <div className="grid grid-cols-2 gap-4 mt-4 ml-6">
       {movieData.map((movie) => (
         <MovieItem setMovieId={setMovieId} key={movie.id} movie={movie} />
       ))}
