abaa5b1df395f78c343ca0a0f8c10a3597261937
Arnel Francia <nelffff@gmail.com>
2024-08-29 20:42:05 +0800

    finally fixed the movie list grid layout

--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -1,12 +1,16 @@
 export default function MovieItem({ movie, setMovieId }) {
   const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
   return (
-    <div className="w-40 bg-slate-400 flex flex-col justify-start items-center p-3 gap-2">
+    <div className="w-40 bg-slate-400 flex flex-col justify-center items-center p-3 gap-2">
       <div>
-        <img className="w-32" src={`${IMAGE_BASE_URL}${movie.poster_path}`} />
+        <img
+          className="w-32"
+          src={`${IMAGE_BASE_URL}${movie.poster_path}`}
+          alt="Poster Unavailable"
+        />
       </div>
       <div>
-        <h1 className="text-sm text-wrap font-semibold">
+        <h1 className="text-sm text-wrap font-semibold 615:text-base">
           {movie.original_title}
         </h1>
       </div>
