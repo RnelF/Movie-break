63b87662bfae87defe2cf148d2123663cf2a84d7
Arnel Francia <nelffff@gmail.com>
2024-09-09 20:53:12 +0800

    removed the edge fade further adjusting the design

--- src/components/TrendingMovieItems.jsx
+++ src/components/TrendingMovieItems.jsx
@@ -1,8 +1,7 @@
-import "../custom_css/edgeFade.css";
 export default function TrendingMovieItems({ movie, setMovieId }) {
   const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
   return (
-    <div className="w-32 500:w-48 615:w-48 bg-black bg-opacity-60 flex flex-col justify-start items-center p-3 gap-2 mb-2 rounded-lg edge-fade">
+    <div className="w-32 500:w-48 615:w-48 bg-black bg-opacity-60 flex flex-col justify-start items-center gap-2  rounded-lg">
       <div className="w-24 500:w-40 615:w-40 h-auto">
         <img
           className="w-full rounded-lg cursor-pointer"
@@ -14,7 +13,7 @@ export default function TrendingMovieItems({ movie, setMovieId }) {
         />
       </div>
       <div>
-        <h1 className="text-base text-wrap font-semibold 500:text-xl 615:text-xl text-gray-200">
+        <h1 className="text-sm text-wrap text-center p-1 font-semibold 500:text-xl 615:text-xl text-gray-200">
           {movie.original_title}
         </h1>
       </div>
