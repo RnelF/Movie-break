868013ec7a00ae9820d3da097b3883e77c9241f9
Arnel Francia <nelffff@gmail.com>
2024-09-16 21:24:39 +0800

    Learning Rest API

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -39,13 +39,13 @@ export default function Nav() {
         } lg:flex flex-col lg:flex-row list-none text-3xl items-center fixed inset-0 justify-center bg-white bg-opacity-90 lg:static lg:bg-transparent ml-auto 1:text-lg 500:text-xl 615:text-2xl 1:w-40 500:w-48 615:w-52 1:h-44 500:h-48 615:h-52 lg:w-auto lg:h-auto 1:rounded-md  top-24 right-16`}
       >
         <ul className="flex flex-col lg:flex-row ">
-          <li className="mb-4 lg:mb-0 lg:mr-4 p-2 font-semibold cursor-pointer hover:text-slate-700 duration-200 shadow-lg">
+          <li className="mb-4 lg:mb-0 lg:mr-4 p-2 font-semibold cursor-pointer hover:text-slate-200 duration-200 shadow-lg">
             Home
           </li>
-          <li className="mb-4 lg:mb-0 lg:mr-4 p-2 font-semibold cursor-pointer hover:text-slate-700 duration-200 shadow-lg">
+          <li className="mb-4 lg:mb-0 lg:mr-4 p-2 font-semibold cursor-pointer hover:text-slate-200 duration-200 shadow-lg">
             Contact
           </li>
-          <li className="mb-4 lg:mb-0 lg:mr-4 p-2 font-semibold cursor-pointer hover:text-slate-700 duration-200 shadow-lg">
+          <li className="mb-4 lg:mb-0 lg:mr-4 p-2 font-semibold cursor-pointer hover:text-slate-200 duration-200 shadow-lg">
             About
           </li>
         </ul>
